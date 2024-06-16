42dc04fd0c798741922e7f0ee2612d5a1ffc2aaa
a <a@as-MacBook-Air.local>
2024-06-16 16:13:47 +0300

    s

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'edf3fd025a127dfc587db22c571d8536637c14a1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'edf3fd025a127dfc587db22c571d8536637c14a1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sqlitecloud/sqlitecloud' => array(
+            'pretty_version' => '0.2.0',
+            'version' => '0.2.0.0',
+            'reference' => '3a6a4ff8a662ee197e6612a0227a2d66efe2a8ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sqlitecloud/sqlitecloud',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
